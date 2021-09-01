e8df587230ef857815e8272e22d426a2fa868cb7
drewandrewd <andrewid10@gmail.com>
2021-09-01 15:04:37 +0500

    new

--- src/test/java/org/example/FirstTest.java
+++ src/test/java/org/example/FirstTest.java
@@ -3,15 +3,13 @@ package org.example;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import javax.swing.*;
 import java.util.concurrent.TimeUnit;
 
 public class FirstTest {
@@ -27,6 +25,7 @@ public class FirstTest {
 
     @Test
     public void test() {
+        //Login
         driver.get("https://crm.geekbrains.space/user/login");
         driver.findElement(By.id("prependedInput")).click();
         driver.findElement(By.id("prependedInput")).sendKeys("Applanatest1");
@@ -37,12 +36,35 @@ public class FirstTest {
         action.moveToElement(driver.findElement(By.xpath("//*[@id=\"main-menu\"]/ul/li[3]/a/span"))).build().perform();
         driver.findElement(By.xpath("//*[@id=\"main-menu\"]/ul/li[3]/ul/li[4]/a")).click();
         driver.findElement(By.xpath("//*[@id=\"container\"]/div[1]/div/div/div[2]/div/div/a")).click();
+        //Create project
         driver.findElement(By.name("crm_project[name]")).click();
         driver.findElement(By.name("crm_project[name]")).sendKeys("new888");
         driver.findElement(By.cssSelector(".select2-default > .select2-arrow")).click();
-        WebDriverWait wait = new WebDriverWait(driver, 3);
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"select2-drop\"]/ul[2]/li[1]")));
-        driver.findElement(By.xpath("//*[@id=\"select2-drop\"]/ul[2]/li[1]")).sendKeys(Keys.ENTER);
-
+        driver.findElement(By.xpath("//*[@id=\"select2-drop\"]/ul[2]/li[1]/div")).click();
+        //Thread.sleep(3000);
+        new WebDriverWait(driver, 3).until(ExpectedConditions.invisibilityOfElementLocated(By.className("select2-container-disabled")));
+        driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/div[3]/form/div[2]/div[3]/div/div[1]/div[2]/fieldset/div[4]/div[1]/div[2]/div/a/span[2]")).click();
+        driver.findElement(By.xpath("//*[@id=\"select2-drop\"]/ul[2]/li[1]/div")).click();
+        {
+            Select select = new Select(driver.findElement(By.name("crm_project[businessUnit]")));
+            select.selectByVisibleText("Research & Development");
+        }
+        {
+            Select select = new Select(driver.findElement(By.name("crm_project[curator]")));
+            select.selectByVisibleText("Applanatest Applanatest Applanatest");
+        }
+        {
+            Select select = new Select(driver.findElement(By.name("crm_project[rp]")));
+            select.selectByVisibleText("Applanatest Applanatest Applanatest");
+        }
+        {
+            Select select = new Select(driver.findElement(By.name("crm_project[administrator]")));
+            select.selectByVisibleText("Applanatest Applanatest Applanatest");
+        }
+        {
+            Select select = new Select(driver.findElement(By.name("crm_project[manager]")));
+            select.selectByVisibleText("Applanatest Applanatest Applanatest");
+        }
+        driver.findElement(By.cssSelector(".btn-group:nth-child(4) > .btn")).click();
     }
 }
